dafe90491279f0e230c0df02c9b542feef770e6c
abduholiqovmuhammadzohir <abduholiqovmuhammadzohir@gmail.com>
2023-12-24 19:35:54 +0500

    homework

---
+++ functions.js
@@ -0,0 +1,11 @@
+function sum(a,b) {
+    return a + b;  
+}
+
+function cube(a) {
+    return a ** 3;
+}
+
+const API = 'https://kun.uz';
+
+export {sum as sumAll,cube,API}
---
+++ index.js
@@ -0,0 +1,115 @@
+// function divide(a,b) {
+//     if (b == 0) {
+//         throw new Error("Ikkinchi son 0 bo'lishi mumkin")
+//     }
+//     return a / b;
+// }
+// try {
+//     divide(10,0)
+// } catch (error) {
+//     console.log(error);
+// }
+
+
+// function year(age) {
+//     let sum = 0
+//     if (b == 0) {
+//         throw new Error("Ikkinchi son 0 bo'lishi mumkin")
+//     }
+//     return a - b;
+// }
+// try {
+//     divide(2023,1)
+// } catch (error) {
+//     console.log(error);
+// }
+
+// console.log(sum);
+
+
+// import { sumAll } from "./functions.js";
+// console.log(sumAll(2,4));
+
+
+// 1 - misol
+
+// let arr = [1,2,3,4]
+
+// function sum(arr) {    
+//     let res = 1;
+//     let res1 = 0;
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] % 2 == 0) {
+//             res = arr[i] ** 2
+//             res1 += res
+//         }
+//     }
+//     return res1
+// }
+// console.log(sum(arr));
+
+// 2 - misol
+
+// let arr = ["salom","hello","bye"]
+
+// function sum(arr) {
+//     let res1 = [];
+
+//     for (let i = 0; i < arr.length; i++) {
+//         let res = arr[i].charAt(0)
+//         res1.push(res)
+//     }
+//     return res1
+// }
+// console.log(sum(arr));
+
+// 3 - misol
+
+// let arr = {
+//     age: 3,
+//     year: 2,
+//     name: "salom",
+//     username: "bye"
+// }
+
+// function sum(arr) {
+//     let res = 0;
+//     for (const key in arr) {
+//         if (key == Number){
+//             res += key
+//         }
+//     }
+//     return res
+// }
+// console.log(sum(arr));
+
+
+// 4 - misol
+
+// let arr = [1,2,3,4,5,6]
+
+// function sum(arr) {
+//     let res = 0
+//     for (let i = 0; i < arr.length ; i++) {
+//         let counter = 0;
+//         for (let j = 0; j < arr.length ; j++) {
+//             if (arr[i] % arr[j] == 0) {
+//                 counter++;
+//             }
+//         }
+//         if (counter == 2) {
+//             res += i
+//         }   
+//     }
+//     return res
+// }
+// console.log(sum(arr));
+
+
+// 5 - misol
+
+//  let arr = 8234556;
+
+//  function sum(arr) {
+//      let 
+//  }
